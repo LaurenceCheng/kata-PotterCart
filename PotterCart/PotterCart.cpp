e1463a519d25db2fc91f1b13c3ceb9bf040f1dd8
Laurence10 <laurence10+github@gmail.com>
2018-02-07 01:09:20 +0800

    [Refactor] modify algorithm.

--- PotterCart/PotterCart.cpp
+++ PotterCart/PotterCart.cpp
@@ -17,15 +17,16 @@ void PotterCart::AddPotterBook(uint32_t volume)
 	m_books[volume]++;
 }
 
-uint32_t PotterCart::GetTotal(void) const
+uint32_t PotterCart::GetTotal(void)
 {
 	if (m_books.size() >= 2)
 	{
-		auto numberOfPair = GetNumberOfPair();
-		auto totalPriceOfPairs = numberOfPair * static_cast<uint32_t>(PRICE_PER_BOOK * 2 * DISCOUNT_FOR_PAIR);
+		auto countOfDuo = GetCountOfDuo();
+		auto totalPriceOfDuos = countOfDuo * static_cast<uint32_t>(PRICE_PER_BOOK * 2 * DISCOUNT_FOR_PAIR);
+		RemoveDuo();
 
-		auto numberOfSingle = GetNumberOfBooks() - 2 * numberOfPair;
-		return totalPriceOfPairs + numberOfSingle * PRICE_PER_BOOK;
+		auto countOfSolo = GetCountOfSolo();
+		return totalPriceOfDuos + countOfSolo * PRICE_PER_BOOK;
 	}
 	else
 	{
@@ -33,19 +34,43 @@ uint32_t PotterCart::GetTotal(void) const
 	}
 }
 
-uint32_t PotterCart::GetNumberOfBooks(void) const
+uint32_t PotterCart::GetCountOfSolo(void) const
 {
-	auto count = 0;
+	auto minCountOfVolume = 0u;
 	for (const auto &volume : m_books)
 	{
-		count += volume.second;
+		minCountOfVolume = std::min(volume.second, minCountOfVolume);
 	}
 
-	return count;
+	return minCountOfVolume;
 }
 
-uint32_t PotterCart::GetNumberOfPair(void) const
+uint32_t PotterCart::GetCountOfDuo(void) const
 {
-	auto minBook = min_element(m_books.cbegin(), m_books.cend(), m_books.value_comp());
-	return minBook->second;
+	auto minCountOfVolume = std::numeric_limits<uint32_t>::max();
+	for (const auto &volume : m_books)
+	{
+		if (volume.second == 0) continue;
+		minCountOfVolume = std::min(volume.second, minCountOfVolume);
+	}
+
+	return minCountOfVolume;
+}
+
+void PotterCart::RemoveDuo(void)
+{
+	auto countOfDuo = GetCountOfDuo();
+	auto iter = m_books.begin();
+	while (iter != m_books.end())
+	{
+		if (iter->second == countOfDuo)
+		{
+			iter = m_books.erase(iter);
+		}
+		else
+		{
+			iter->second -= countOfDuo;
+			iter++;
+		}
+	}
 }
--- PotterCart/PotterCart.h
+++ PotterCart/PotterCart.h
@@ -9,15 +9,16 @@ public:
 	~PotterCart(void);
 
 	void AddPotterBook(uint32_t volume);
-	uint32_t GetTotal(void) const;
+	uint32_t GetTotal(void);
 
 private:
 	static int PRICE_PER_BOOK;
 	static double DISCOUNT_FOR_PAIR;
 
 private:
-	uint32_t GetNumberOfBooks(void) const;
-	uint32_t GetNumberOfPair(void) const;
+	uint32_t GetCountOfSolo(void) const;
+	uint32_t GetCountOfDuo(void) const;
+	void RemoveDuo(void);
 
 	std::map<uint32_t, uint32_t> m_books;
 };
